c34a0944db8cefe843cf3f9c18a260c5501215db
yoghantara08 <gyoghantara@gmail.com>
2025-06-13 13:39:57 +0800

    primitive types

--- exercises/04_primitive_types/primitive_types2.rs
+++ exercises/04_primitive_types/primitive_types2.rs
@@ -18,6 +18,8 @@ fn main() {
     // from a different language than your own, try an emoji 😉
     // let your_character = '';
 
+    let your_character = 'T';
+
     if your_character.is_alphabetic() {
         println!("Alphabetical!");
     } else if your_character.is_numeric() {
